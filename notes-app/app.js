03759c8a5a92ef0f3910ae4625b5262ff813e173
didier <dyder1493@gmail.com>
2019-05-06 19:02:56 +0200

    isntall modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,12 @@
 // récupère les variables du fichier dans utilsVar
-const utilsVar = require('./utils.js')
+//const utilsVar = require('./utils.js')
+//console.log(utilsVar.concatName)
 
-console.log(utilsVar.concatName)
+
+const validator = require('validator')
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+console.log(msg)
+
+console.log(validator.isURL('https://www.qwant.com/?q=npm+js&client=ext-chrome-sb'))
