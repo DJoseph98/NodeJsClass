b6bdb98b48ee7595c63dc973f41f8f5bf9415036
didier <dyder1493@gmail.com>
2019-05-07 21:15:08 +0200

    voila

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,13 @@
 // récupère les variables du fichier dans utilsVar
 //const utilsVar = require('./utils.js')
 //console.log(utilsVar.concatName)
-
-
-const validator = require('validator')
+const chalk = require('chalk');
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 console.log(msg)
 
-console.log(validator.isURL('https://www.qwant.com/?q=npm+js&client=ext-chrome-sb'))
+//console.log(validator.isURL('https://www.qwant.com/?q=npm+js&client=ext-chrome-sb'))
+
+console.log(chalk.red.inverse('Error'));
+
