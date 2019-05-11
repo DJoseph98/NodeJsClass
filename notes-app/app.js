503fc363297cf83d03a87109c629608e93758b79
didier <dyder1493@gmail.com>
2019-05-11 12:25:33 +0200

    exemple progress.argv écriture argument dans console

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,10 +4,20 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
+/*const msg = getNotes()
 console.log(msg)
 
 //console.log(validator.isURL('https://www.qwant.com/?q=npm+js&client=ext-chrome-sb'))
 
 console.log(chalk.red.inverse('Error'));
+console.log(process.argv[2])*/
+console.log(process.argv)
+const command = process.argv[2]
 
+switch (command) {
+    case 'add':
+        console.log('add note !')
+        break;
+    case 'remove':
+        console.log('remove note ...')
+}
