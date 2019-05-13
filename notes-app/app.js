90f99111824ef252a88d970ed6bfb2081259b178
didier <dyder1493@gmail.com>
2019-05-13 12:21:34 +0200

    continution

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,21 +3,43 @@
 //console.log(utilsVar.concatName)
 const chalk = require('chalk');
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
-/*const msg = getNotes()
-console.log(msg)
+//create add command
 
-//console.log(validator.isURL('https://www.qwant.com/?q=npm+js&client=ext-chrome-sb'))
+yargs.version('1.1.0')
 
-console.log(chalk.red.inverse('Error'));
-console.log(process.argv[2])*/
-console.log(process.argv)
-const command = process.argv[2]
+//Add command
+yargs.command({
+    command: 'add',
+    describe: 'Add new note',
+    handler: function () {
+        console.log('Adding note')
+    }
+})
 
-switch (command) {
-    case 'add':
-        console.log('add note !')
-        break;
-    case 'remove':
-        console.log('remove note ...')
-}
+yargs.command({
+    command: 'remove',
+    describe: 'removing note',
+    handler: function () {
+        console.log('note removed')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'list notes',
+    handler: function () {
+        console.log('note list :')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'remove note',
+    handler: function () {
+        console.log('read note')
+    }
+})
+
+console.log(yargs.argv)
