d30dbad30973635b2947b959c5a9885387af4c67
Didier <d.joseph@cruiseline.eu>
2019-05-13 14:57:00 +0200

    excercice lecture + réécriture sur un fichier json

--- notes-app/fun/1-json.js
+++ notes-app/fun/1-json.js
@@ -1,6 +1,12 @@
 const fs = require('fs')
 
-const dataBuffer = fs.readFileSync('1-json.json') // récupère fichier
-const dataJSON = dataBuffer.toString() // convertie en string
-const data = JSON.parse(dataJSON) // parse en objet
-console.log(data)
+// const dataBuffer = fs.readFileSync('1-json.json') // récupère fichier
+// const dataJSON = dataBuffer.toString() // convertie en string
+// const data = JSON.parse(dataJSON) // parse en objet
+// console.log(data)
+
+const data = JSON.parse(fs.readFileSync('1-json.json').toString())
+data.name="didier"
+data.age=26
+dataConvertToSave = JSON.stringify(data)
+fs.writeFileSync('1-json.json', JSON.stringify(data))
