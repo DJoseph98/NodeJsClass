434ac49708885bc7e18e0da4b8c7b44580a2820d
Didier <d.joseph@cruiseline.eu>
2019-05-16 11:38:01 +0200

    exercice création callback fonction forecast

--- meteo-app/app.js
+++ meteo-app/app.js
@@ -1,6 +1,12 @@
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 geocode('Monaco', (error, data) => {  // convention 2 arguments error + data pour des fonctions de callback
+    console.log("Error", error)
+    console.log("Data", data)
+})
+
+forecast(43, 7, (error, data) => {  // convention 2 arguments error + data pour des fonctions de callback
     console.log("Error", error)
     console.log("Data", data)
 })
