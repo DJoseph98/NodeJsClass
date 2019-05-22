ed4d9a6a146d74f4acd03f9dafbad28e4ac8a1cf
Didier <d.joseph@cruiseline.eu>
2019-05-22 11:00:17 +0200

    exo + création form météo + retrieve data

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,9 @@
-console.log('Client side javascript')
+const weatherForm = document.querySelector('form') //selectionne le formulaire form
+const locationInput = document.querySelector('input') // selectionne l'input du formulaire
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-fetch('http://localhost:3000/weather?adress=Boston').then((response) => {
+const fecthForecast = (location) => {
+    const locationEncode = encodeURI(location)
+    fetch('http://localhost:3000/weather?adress=' + locationEncode).then((response) => {
         response.json().then( (data) => {
             if(data.error){
                 console.log(data.error)
@@ -15,4 +12,13 @@ fetch('http://localhost:3000/weather?adress=Boston').then((response) => {
             }
 
         })
+    })
+}
+
+weatherForm.addEventListener( 'submit', (e) => { // creation evenement submit form
+    e.preventDefault()      //permet de ne pas réactualiser la page après submit
+    const location = locationInput.value
+
+    fecthForecast(location)
+
 })
